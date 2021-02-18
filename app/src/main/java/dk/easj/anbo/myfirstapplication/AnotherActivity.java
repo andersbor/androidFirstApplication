06bf4baec4f34fb53a4c9e318ef61ed527dd5dd0
Anders Børjesson <anbo@zealand.dk>
2021-02-18 18:23:12 +0100

    Gradle update

--- app/src/main/java/dk/easj/anbo/myfirstapplication/AnotherActivity.java
+++ app/src/main/java/dk/easj/anbo/myfirstapplication/AnotherActivity.java
@@ -1,8 +1,7 @@
 package dk.easj.anbo.myfirstapplication;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,13 +9,6 @@ import android.view.View;
 public class AnotherActivity extends AppCompatActivity {
     private static final String EMAIL = "sdksfkdfksdk";
     private static final String SHOE_SIZE = "shoeSize";
-
-    public static Intent getIntent(Context from, String email, int shoeSize) {
-        Intent intent = new Intent(from, AnotherActivity.class);
-        intent.putExtra(AnotherActivity.EMAIL, email);
-        intent.putExtra(AnotherActivity.SHOE_SIZE, shoeSize);
-        return intent;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +22,6 @@ public class AnotherActivity extends AppCompatActivity {
     }
 
     public void goBackPressed(View view) {
-        // add data
-        Intent intent = new Intent();
-        intent.putExtra("username", "anders");
-        intent.putExtra("password", "secret12");
-        setResult(RESULT_CANCELED, intent);
         finish();
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        goBackPressed(null);
     }
 }
--- app/src/main/java/dk/easj/anbo/myfirstapplication/MainActivity.java
+++ app/src/main/java/dk/easj/anbo/myfirstapplication/MainActivity.java
@@ -1,52 +1,28 @@
 package dk.easj.anbo.myfirstapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final int REQUEST_CODE_USERNAME_PASSWORD = 14593;
-    private int counter = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void buttonCliced(View view) {
+    public void buttonClicked(View view) {
         TextView textView = findViewById(R.id.mainMessageTextView);
-        counter++;
-
         EditText editText = findViewById(R.id.mainEmailEditText);
         String email = editText.getText().toString();
-        textView.setText("Hallo  " + email);
-
+        textView.setText("Hello " + email);
     }
 
     public void buttonGoClicked(View view) {
-        EditText editText = findViewById(R.id.mainEmailEditText);
-        String email = editText.getText().toString();
-        Intent intent = AnotherActivity.getIntent(this, email, 42);
-        startActivityForResult(intent, REQUEST_CODE_USERNAME_PASSWORD);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_USERNAME_PASSWORD) {
-            if (resultCode == RESULT_OK) {
-                String username = data.getStringExtra("username");
-                String password = data.getStringExtra("password");
-                Log.d("MINE", username + " " + password);
-            } else {
-                Log.d("MINE", "You cancelled");
-            }
-        }
-
+        Intent intent = new Intent(this, AnotherActivity.class);
+        startActivity(intent);
     }
 }
